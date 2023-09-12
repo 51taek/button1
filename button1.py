dc6a99829efb54f6fe48421c5bc64a546b90f611
51taek <wontaek88@gmail.com>
2023-09-12 19:27:03 +0900

    button update

--- button1.py
+++ button1.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 window =Tk()
 
-b1 = Button(window, text="wontaek Oh")
+b1 = Button(window, text="wontaekOh")
 b2 = Button(window, text="60202220")
 b1.grid(row=0, column=0)
 b2.grid(row=1, column=1)
